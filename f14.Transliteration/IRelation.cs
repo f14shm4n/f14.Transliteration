8154ef4198737caf940deeee93b0336289286418
f14shm4n <uraborg@gmail.com>
2018-01-17 17:21:39 +0300

    Ready to release.

--- f14.Transliteration/IRelation.cs
+++ f14.Transliteration/IRelation.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace f14.Transliteration
 {
+    /// <summary>
+    /// Represents the char relation.
+    /// </summary>
     public interface IRelation
-    {     
+    {
+        /// <summary>
+        /// The relation table.
+        /// </summary>
         IDictionary<string, string> Table { get; }
     }
 }
